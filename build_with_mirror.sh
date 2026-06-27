4022d5a2faa7324882630958cf52538cc791af6d
bowenzhang01 <bowen01.zhang@outlook.com>
2026-06-27 15:48:54 +0800

    Cleanup: remove personal profiles/characters, update default to dorm_life

--- build_with_mirror.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Buildozer wrapper with PyPI mirror for WSL
-# Use: source this file or run: bash build_with_mirror.sh
-export PIP_INDEX_URL=https://pypi.tuna.tsinghua.edu.cn/simple
-export PIP_TRUSTED_HOST=pypi.tuna.tsinghua.edu.cn
-echo "Using PyPI mirror: $PIP_INDEX_URL"
-buildozer android debug
